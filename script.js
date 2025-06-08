f6dbf7f49bf2b9125a5a580ccff390b04634e8d4
SUPRIYA MISHRA <140587863+supiimishra01@users.noreply.github.com>
2025-06-08 16:55:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+
+
+// Get the element by its ID
+let element = document.getElementById("level");
+
+// Initialize level counter
+let level = 0;
+
+// Traverse up the DOM tree until there are no more parents
+while (element) {
+    level++;
+    element = element.parentElement;
+}
+
+// Show the result
+alert("The level of the element is: " + level);
+
